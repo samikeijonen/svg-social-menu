ec55be34bbe4cc92d7a23a63b5c2968d2900d3ca
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-02-18 19:52:31 +0200

    remove extra SVGs.

--- svg-icons.php
+++ svg-icons.php
@@ -13,26 +13,6 @@
 <desc id="icon-rating-full-desc"><?php esc_html_e( 'rating svg icon', 'svg-social-menu'); ?></desc>
 <path class="path1" d="M960 384h-342.656l-105.344-320-105.344 320h-342.656l277.632 193.984-106.496 318.016 276.864-197.248 276.864 197.248-106.432-318.016z"></path>
 </symbol>
-<symbol id="icon-checkmark" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="checkmark-title"><?php esc_html_e( 'checkmark', 'svg-social-menu'); ?></title>
-<desc id="checkmark-desc"><?php esc_html_e( 'checkmark svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M704 272l-224 256-112-96-64 64 144 208h64l256-384z"></path>
-</symbol>
-<symbol id="icon-collapse" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="collapse-title"><?php esc_html_e( 'collapse', 'svg-social-menu'); ?></title>
-<desc id="collapse-desc"><?php esc_html_e( 'collapse svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M704 704l-224-224-224 224-64-64 288-288 288 288z"></path>
-</symbol>
-<symbol id="icon-edit" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="edit-title"><?php esc_html_e( 'edit', 'svg-social-menu'); ?></title>
-<desc id="edit-desc"><?php esc_html_e( 'edit svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M640 160l-448 448v224h224l448-448-224-224zM640 256l48 48-297.344 297.344-48-48 297.344-297.344zM384 768l-128-128 64-64 128 128-64 64zM422.656 633.344l297.344-297.344 48 48-297.344 297.344-48-48z"></path>
-</symbol>
-<symbol id="icon-expand" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="expand-title"><?php esc_html_e( 'expand', 'svg-social-menu'); ?></title>
-<desc id="expand-desc"><?php esc_html_e( 'expand svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M704 384l-224 224-224-224-64 64 288 288 288-288z"></path>
-</symbol>
 <symbol id="icon-facebook" viewBox="0 0 1024 1024" aria-labelledby="title desc">
 <title id="facebook-title"><?php esc_html_e( 'facebook', 'svg-social-menu'); ?></title>
 <desc id="facebook-desc"><?php esc_html_e( 'facebook svg icon', 'svg-social-menu'); ?></desc>
@@ -53,50 +33,10 @@
 <desc id="mail-desc"><?php esc_html_e( 'mail svg icon', 'svg-social-menu'); ?></desc>
 <path class="path1" d="M128 256v64l384 192 384-192v-64h-768zM128 416v352h768v-352l-384 192-384-192z"></path>
 </symbol>
-<symbol id="icon-month" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="month-title"><?php esc_html_e( 'month', 'svg-social-menu'); ?></title>
-<desc id="month-desc"><?php esc_html_e( 'month svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M384 704h64v-64h-64v64zM384 576h64v-64h-64v64zM384 448h64v-64h-64v64zM256 704h64v-64h-64v64zM256 576h64v-64h-64v64zM640 576h64v-64h-64v64zM512 448h64v-64h-64v64zM640 448h64v-64h-64v64zM704 192v-64h-64v64h-320v-64h-64v64h-128v640h704v-640h-128zM768 704c0 35.392-28.608 64-64 64h-448c-35.328 0-64-28.608-64-64v-320c0-35.392 28.672-64 64-64h448c35.392 0 64 28.608 64 64v320zM512 576h64v-64h-64v64zM512 704h64v-64h-64v64z"></path>
-</symbol>
-<symbol id="icon-next" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="next-title"><?php esc_html_e( 'next', 'svg-social-menu'); ?></title>
-<desc id="next-desc"><?php esc_html_e( 'next svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M512 297.344l-43.328 43.328 171.328 171.328h-448v64h448l-171.328 171.328 43.328 43.328 246.656-246.656z"></path>
-</symbol>
-<symbol id="icon-phone" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="phone-title"><?php esc_html_e( 'phone', 'svg-social-menu'); ?></title>
-<desc id="phone-desc"><?php esc_html_e( 'phone svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M640 128h-256c-70.656 0-128 57.344-128 128v512c0 70.656 57.344 128 128 128h256c70.656 0 128-57.344 128-128v-512c0-70.656-57.344-128-128-128zM512 832c-35.328 0-64-28.608-64-64s28.672-64 64-64c35.392 0 64 28.608 64 64s-28.608 64-64 64zM704 640h-384v-384h384v384z"></path>
-</symbol>
-<symbol id="icon-previous" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="previous-title"><?php esc_html_e( 'previous', 'svg-social-menu'); ?></title>
-<desc id="previous-desc"><?php esc_html_e( 'previous svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M512 297.344l43.328 43.328-171.328 171.328h448v64h-448l171.328 171.328-43.328 43.328-246.656-246.656z"></path>
-</symbol>
-<symbol id="icon-search" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="search-title"><?php esc_html_e( 'search', 'svg-social-menu'); ?></title>
-<desc id="search-desc"><?php esc_html_e( 'search svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M896 832l-247.424-247.424c34.496-47.424 55.424-105.408 55.424-168.576 0-159.040-128.96-288-288-288s-288 128.96-288 288 128.96 288 288 288c63.168 0 121.152-20.928 168.576-55.424l247.424 247.424 64-64zM192 416c0-123.712 100.288-224 224-224 123.776 0 224 100.288 224 224s-100.224 224-224 224c-123.712 0-224-100.288-224-224z"></path>
-</symbol>
 <symbol id="icon-twitter" viewBox="0 0 1024 1024" aria-labelledby="title desc">
 <title id="twitter-title"><?php esc_html_e( 'twitter', 'svg-social-menu'); ?></title>
 <desc id="twitter-desc"><?php esc_html_e( 'twitter svg icon', 'svg-social-menu'); ?></desc>
 <path class="path1" d="M960 232.128c-32.96 14.656-68.352 24.512-105.536 28.928 37.888-22.72 67.072-58.752 80.768-101.696-35.456 21.056-74.816 36.352-116.608 44.608-33.536-35.712-81.344-58.048-134.272-58.048-101.504 0-183.808 82.304-183.808 183.808 0 14.4 1.664 28.416 4.8 41.856-152.768-7.68-288.256-80.832-378.944-192.064-15.808 27.136-24.896 58.816-24.896 92.48 0 63.744 32.384 120 81.728 152.96-30.080-0.96-58.432-9.216-83.264-22.976 0 0.832 0 1.536 0 2.368 0 89.088 63.296 163.328 147.456 180.224-15.424 4.16-31.616 6.4-48.384 6.4-11.904 0-23.36-1.28-34.624-3.264 23.424 73.088 91.328 126.144 171.712 127.552-62.848 49.344-142.208 78.72-228.224 78.72-14.848 0-29.504-0.896-43.904-2.624 81.344 52.224 177.92 82.624 281.728 82.624 338.176 0 523.136-280.128 523.136-523.008 0-7.936-0.256-15.872-0.64-23.744 35.904-25.92 67.136-58.24 91.776-95.104z"></path>
-</symbol>
-<symbol id="icon-video" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="video-title"><?php esc_html_e( 'video', 'svg-social-menu'); ?></title>
-<desc id="video-desc"><?php esc_html_e( 'video svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M256 192v640l576-320z"></path>
-</symbol>
-<symbol id="icon-uparrow" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="uparrow-title"><?php esc_html_e( 'uparrow', 'svg-social-menu'); ?></title>
-<desc id="uparrow-desc"><?php esc_html_e( 'uparrow svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M1024 1024l-480-479.936-480 479.936z"></path>
-</symbol>
-<symbol id="icon-downarrow" viewBox="0 0 1024 1024" aria-labelledby="title desc">
-<title id="downarrow-title"><?php esc_html_e( 'downarrow', 'svg-social-menu'); ?></title>
-<desc id="downarrow-desc"><?php esc_html_e( 'downarrow svg icon', 'svg-social-menu'); ?></desc>
-<path class="path1" d="M64 0l480 480 480-480z"></path>
 </symbol>
 </defs>
 </svg>
